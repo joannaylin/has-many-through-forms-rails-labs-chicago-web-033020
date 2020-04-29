185de1ee876bec306049432a4b9da784d2127507
Joanna Lin <lin.joanna.y@gmail.com>
2020-04-29 09:50:02 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: "bloopy", email: "bloooop@gmail.com")
+Post.create(title: "and then there were none", content: "idk man")
+Category.create(name: "Horror" )
+Comment.create(content: "BOOOO!")
